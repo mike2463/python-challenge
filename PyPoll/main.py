a5bfe0356d1487aebd10425758d39f42f132983f
mike2463 <michaeljs90@msn.com>
2023-03-19 13:32:21 -0500

    commented and cleaned PyPoll script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,7 +9,9 @@ candidate_list = []
 #import csv file 
 election_data = os.path.join('Resources', 'election_data.csv')
 
+#Open a text document
 results = open('results.txt', 'w')
+
 #open csv file and create variables that will store data as the average P/L is being calculated
 with open(election_data,'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
@@ -18,38 +20,40 @@ with open(election_data,'r') as csvfile:
 #convert csvreader to a stored list
     candidates = list(csvreader)
     
-#Loop through budgetlist to find the total months, net total amount, changes in P/L and average of P/L, the greatest increase/decrease
+#Loop through candidates list to total the number of votes and then to create a list of candidates
     for row in candidates:
         totalvotes = totalvotes + 1
         candidate_list += [row[2]]
-        #if candidate_list == 'Charles Casper Stockham':
-           # charles += candidate_list
-   
+      
+#Filter through the candidates list and create a list of each candidate
     charles_list = list(filter(lambda i: 'Charles Casper Stockham' in i, candidate_list))
     diana_list = list(filter(lambda i: 'Diana DeGette' in i, candidate_list))
     raymon_list = list(filter(lambda i: 'Raymon Anthony Doane' in i, candidate_list))
+
+#Find the total number of votes for each candidate based on the length of the list for each candidate
     charles = len(charles_list)
     diana = len(diana_list)
     raymon = len(raymon_list)
 
+#Calculate the the percentage for each candidate
     percent_charles = round((charles/totalvotes)*100, 3)
     percent_diana = round((diana/totalvotes)*100, 3)
     percent_raymon = round((raymon/totalvotes)*100, 3)
 
+#Create dictionary with the results of candidate
     election_results = {'Charles': percent_charles, 'Diana': percent_diana, 'Raymon': percent_raymon}
-    
+
+#Create a new variable that stores the winner of the election by using the election_results dictionary and the max function 
     winner = max(election_results, key=election_results.get)
         
-#print all of the calculations   
+#print all of the results   
     print(totalvotes)
     print(percent_charles, charles)
     print(percent_diana, diana)
     print(percent_raymon, raymon)
     print(winner)
-    
 
 #write the report to a text file. 
-
     results.write(f'Election Results\n-------------\n')
     results.write(f'Total Votes: {totalvotes}\n--------------\n')
     results.write(f'Charles Casper Stockham: {percent_charles}% ({charles})\n')
