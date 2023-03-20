2af7405df7239f7c194d81ad0372fc3a0b43c012
mike2463 <michaeljs90@msn.com>
2023-03-20 18:33:02 -0500

    fixed the max and min

--- PyBank/main.py
+++ PyBank/main.py
@@ -26,26 +26,30 @@ with open(budget_data,'r') as csvfile:
     for row in budgetlist:
         monthcount = monthcount + 1
         total += int(row[1])
-        monthly_change = prevnet - int(row[1])
+        monthly_change = int(row[1]) - prevnet
         prevnet = int(row[1])
         monthly_change_list += [monthly_change]
-        maxchange = max(budgetlist)
-        minchange = min(budgetlist)
+        maxchange = max(monthly_change_list)
+        minchange = min(monthly_change_list)
+        maax = monthly_change_list.index(maxchange)
+        miin = monthly_change_list.index(minchange)
     net_monthly_avg = sum(monthly_change_list)/len(monthly_change_list)
 #print all of the calculations   
     print('Total Months:', monthcount)
     print('Total Profits/Losses:', total)
     print('Changes in P/L:', sum(monthly_change_list))
     print('Average P/L:', net_monthly_avg )
-    print('Greatest increase:', maxchange)
-    print('Greatest Decrease:', minchange)
+    print('Greatest increase:',budgetlist[maax-1][0], maxchange)
+    print('Greatest Decrease:', budgetlist[miin-1][0], minchange)
+    
+
 #write the report to a text file. 
 results.write(f'Financial Analysis\n---------------\n')
 results.write(f'Profit: ${total}\n')
 results.write(f'Total Months: {monthcount}\n')
 results.write(f'Average P/L: ${net_monthly_avg}\n')
-results.write(f'Greatest Increase in Profits: ${maxchange}\n')
-results.write(f'Greatest Decrease in profits: ${minchange}')
+results.write(f'Greatest Increase in Profits: ${budgetlist[maax-1][0]} {maxchange}\n')
+results.write(f'Greatest Decrease in profits: ${budgetlist[miin-1][0]} {minchange}')
         
 
     
